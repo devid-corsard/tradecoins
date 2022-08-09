91df4c25d55b7028dd28819201871d90abb239b9
devid-corsard <truegrinde@gmail.com>
2022-08-09 23:31:26 +0300

    refactor state to a redux-like store with methods

--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import state, {
-  addNewCoin,
-  addNewTrade,
-  editCoinName,
-  editInput,
-  subscribe,
-} from './redux/state';
+import store from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 export const rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
       <App
-        state={state}
-        addNewCoin={addNewCoin}
-        addNewTrade={addNewTrade}
-        editInput={editInput}
-        editCoinName={editCoinName}
+        state={store.getState()}
+        addNewCoin={store.addNewCoin.bind(store)}
+        addNewTrade={store.addNewTrade.bind(store)}
+        editInput={store.editInput.bind(store)}
+        editCoinName={store.editCoinName.bind(store)}
       />
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,69 +1,72 @@
-let rerenderEntireTree = () => {
-  console.log('state changed');
-};
-
 const EMPTY_TRADE = { tradeId: '0', amount: '', buyPrice: '', sellPrice: '' };
 const EMPTY_COIN = { coinId: '', name: '', trades: [] };
 
-let state = {
-  tablePage: {
-    table: [
-      {
-        coinId: 0,
-        name: 'eth',
-        trades: [
-          { tradeId: 0, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
-          { tradeId: 1, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
-          { tradeId: 2, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
-        ],
-      },
-      {
-        coinId: 1,
-        name: 'ltc',
-        trades: [
-          { tradeId: 0, amount: '2', buyPrice: '100', sellPrice: '120' },
-          { tradeId: 1, amount: '2', buyPrice: '110', sellPrice: '' },
-          { tradeId: 2, amount: '2', buyPrice: '120', sellPrice: '' },
-        ],
-      },
-      {
-        coinId: 2,
-        name: 'dot',
-        trades: [
-          { tradeId: 0, amount: '5', buyPrice: '24', sellPrice: '12' },
-          { tradeId: 1, amount: '5', buyPrice: '8', sellPrice: '' },
-          { tradeId: 2, amount: '5', buyPrice: '6', sellPrice: '9' },
-        ],
-      },
-    ],
+let store = {
+  _state: {
+    tablePage: {
+      table: [
+        {
+          coinId: 0,
+          name: 'eth',
+          trades: [
+            { tradeId: 0, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
+            { tradeId: 1, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
+            { tradeId: 2, amount: '0.02', buyPrice: '1800', sellPrice: '1900' },
+          ],
+        },
+        {
+          coinId: 1,
+          name: 'ltc',
+          trades: [
+            { tradeId: 0, amount: '2', buyPrice: '100', sellPrice: '120' },
+            { tradeId: 1, amount: '2', buyPrice: '110', sellPrice: '' },
+            { tradeId: 2, amount: '2', buyPrice: '120', sellPrice: '' },
+          ],
+        },
+        {
+          coinId: 2,
+          name: 'dot',
+          trades: [
+            { tradeId: 0, amount: '5', buyPrice: '24', sellPrice: '12' },
+            { tradeId: 1, amount: '5', buyPrice: '8', sellPrice: '' },
+            { tradeId: 2, amount: '5', buyPrice: '6', sellPrice: '9' },
+          ],
+        },
+      ],
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log('state changed');
+  },
+  addNewCoin() {
+    const table = this._state.tablePage.table;
+    table.push({
+      ...EMPTY_COIN,
+      coinId: table.length,
+      trades: [{ ...EMPTY_TRADE }],
+    });
+    this._callSubscriber(this._state);
+  },
+  addNewTrade(coinId) {
+    const trades = this._state.tablePage.table[coinId].trades;
+    trades.push({ ...EMPTY_TRADE, tradeId: trades.length });
+    this._callSubscriber(this._state);
+  },
+  editInput(coinId, tradeId, field, value) {
+    if (!+value && +value !== 0) return;
+    this._state.tablePage.table[coinId].trades[tradeId][field] = value;
+    this._callSubscriber(this._state);
+  },
+  editCoinName(coinId, value) {
+    this._state.tablePage.table[coinId].name = value;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export const addNewCoin = () => {
-  const table = state.tablePage.table;
-  table.push({
-    ...EMPTY_COIN,
-    coinId: table.length,
-    trades: [{ ...EMPTY_TRADE }],
-  });
-  rerenderEntireTree(state);
-};
-export const addNewTrade = (coinId) => {
-  const trades = state.tablePage.table[coinId].trades;
-  trades.push({ ...EMPTY_TRADE, tradeId: trades.length });
-  rerenderEntireTree(state);
-};
-export const editInput = (coinId, tradeId, field, value) => {
-  if (!+value && +value !== 0) return;
-  state.tablePage.table[coinId].trades[tradeId][field] = value;
-  rerenderEntireTree(state);
-};
-export const editCoinName = (coinId, value) => {
-  state.tablePage.table[coinId].name = value;
-  rerenderEntireTree(state);
-};
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
