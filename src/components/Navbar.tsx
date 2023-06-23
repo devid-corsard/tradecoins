ccbf49f69f71017c8a2ba7d206b0dbe8985170c4
devid-corsard <truegrinde@gmail.com>
2023-06-23 14:24:52 +0300

    small ui fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,10 @@ const Navbar = () => {
   return (
     <div className="w-full mx-auto bg-emerald-950 shadow-xl p-3 m-3 mt-0">
       <h1
-        className="md:text-4xl text-3xl mx-auto mb-3 font-semibold text-amber-500 border-4 py-2 px-4 w-max border-amber-500"
+        className={`md:text-4xl text-3xl mx-auto mb-3 font-semibold text-amber-500 border-4
+        py-2 px-4 w-max border-amber-500
+        cursor-pointer
+        `}
         onClick={() => {
           navigate("/");
         }}
