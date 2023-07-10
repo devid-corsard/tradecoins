8e0af9cc220bca2083340d5d02e947bc31b2ae8a
devid-corsard <truegrinde@gmail.com>
2023-07-10 15:57:32 +0300

    scripts: update env with cargo bin path

--- server/scripts/init_db_prod.sh
+++ server/scripts/init_db_prod.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -x
 set -eo pipefail
+export PATH=$PATH:~/.cargo/bin
 
 if ! [ -x "$(command -v psql)" ]; then
   echo  "Error: psql is not installed."
   exit 1
 fi
 
-# if ! [ -x "$(command -v sqlx)" ]; then
-#   echo  "Error: sqlx is not installed."
-#   echo  "Use:"
-#   echo  "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
-#   echo  "to install it."
-#   exit 1
-# fi
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo  "Error: sqlx is not installed."
+  echo  "Use:"
+  echo  "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+  echo  "to install it."
+  exit 1
+fi
 
 until psql -d postgres -c '\c'
 do
@@ -23,7 +24,6 @@ done
 
 echo "Postgres is up and running - running migrations now!"
 
-cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres
 sqlx database create
 sqlx migrate run
 
