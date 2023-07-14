8447da9c94061a5eb5670d7d4184f968d4b0f1e0
devid-corsard <truegrinde@gmail.com>
2023-07-14 15:31:40 +0300

    script: fixed local db script

--- server/scripts/init_db.sh
+++ server/scripts/init_db.sh
@@ -15,9 +15,9 @@ if ! [ -x "$(command -v sqlx)" ]; then
   exit 1
 fi
 
-DB_USER=${POSTGRES_USER:=postgres}
+DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=tradecoins_v0}"
+DB_NAME="${POSTGRES_DB:=tradecoins_v1}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
@@ -28,7 +28,7 @@ then
       -e POSTGRES_PASSWORD=${DB_PASSWORD} \
       -e POSTGRES_DB=${DB_NAME} \
       -p "${DB_PORT}":5432 \
-      -d postgres \
+      -d postgres:15-alpine \
       postgres -N 1000
 fi
 
