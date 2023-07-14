294a24dfcfcee6ab94e7ce354b8d7d97fbceb0d4
devid-corsard <truegrinde@gmail.com>
2023-07-14 17:17:22 +0300

    server: added request origin to config;

--- server/src/startup.rs
+++ server/src/startup.rs
@@ -2,18 +2,21 @@ use std::net::TcpListener;
 
 use actix_cors::Cors;
 
-use actix_web::{dev::Server, web, App, HttpServer, http};
+use actix_web::{dev::Server, http, web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{health_check, subscribe, register_user};
+use crate::{
+    configuration::get_configuration,
+    routes::{health_check, register_user, subscribe},
+};
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
+    let configuration = get_configuration().expect("Failed to read configuration");
     let server = HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:8080")
-            .allowed_origin("http://127.0.0.1:5173")
+            .allowed_origin(&configuration.application.request_origin)
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
