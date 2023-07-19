ba96fdd4e8ffa7911f615902431e8eed6f92c1db
devid-corsard <truegrinde@gmail.com>
2023-07-19 12:38:13 +0300

    run: delete pm2 scripts/configs;

--- server/scripts/app_start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -eo pipefail
-
-pm2 start ~/rustapps/tradecoins_gha/ecosystem.config.js
--- server/scripts/app_stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -eo pipefail
-
-pm2 stop tradecoins_server
