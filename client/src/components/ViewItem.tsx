f1864f459b8aaee9574347ba89f2941daa4ea03f
devid-corsard <truegrinde@gmail.com>
2023-09-02 17:16:08 +0300

    fix: del unused

--- client/src/components/ViewItem.tsx
+++ client/src/components/ViewItem.tsx
@@ -2,17 +2,7 @@ type Props = {
   name: string;
   value: string;
 };
-const colorMap = {
-  ["Spend:"]: "text-red-500",
-  ["Recieved:"]: "text-emerald-500",
-  ["Difference:"]: "text-amber-500",
-};
 const ViewItem = (props: Props) => {
-  let spanTextColor = "text-slate-300";
-  let spanBgColor = "bg-slate-700";
-  spanTextColor = colorMap[props.name as keyof typeof colorMap];
-  if (Number(props.value) < 0) spanBgColor = "bg-red-900";
-
   return (
     <li>
       <h4>{props.name}</h4>
