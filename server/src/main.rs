b872ca4734edcf94b6f93d6ebd0f0f036374cec8
devid-corsard <truegrinde@gmail.com>
2023-09-03 13:51:24 +0300

    feat: added telemetry

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,14 @@
-use tradecoins::{configuration::get_configuration, startup::Application};
+use tradecoins::{
+    configuration::get_configuration,
+    startup::Application,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let subscriber = get_subscriber("tradecoins".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let application = Application::build(configuration).await?;
     application.run_until_stopped().await?;
