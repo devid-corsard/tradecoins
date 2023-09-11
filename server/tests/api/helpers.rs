5f5ce587004fefb8dc1e79f45096ceb80bd96ba4
devid-corsard <truegrinde@gmail.com>
2023-09-11 17:31:00 +0300

    feat: added telemetry

--- server/tests/api/helpers.rs
+++ server/tests/api/helpers.rs
@@ -1,8 +1,10 @@
 use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use tradecoins::{
     configuration::{get_configuration, DatabaseSettings},
     startup::{get_connection_pool, Application},
+    telemetry::{get_subscriber, init_subscriber},
 };
 use uuid::Uuid;
 
@@ -109,7 +111,22 @@ impl TestUser {
     }
 }
 
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber_name = "test".into();
+    let default_filter_level = "debug".into();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
+
 pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
         c.database.database_name = Uuid::new_v4().to_string();
