5e62784ff632674a52982089eab824d010145159
devid-corsard <truegrinde@gmail.com>
2023-09-11 16:04:26 +0300

    fix: typo

--- server/tests/api/user.rs
+++ server/tests/api/user.rs
@@ -6,7 +6,7 @@ use crate::helpers::spawn_app;
 async fn after_login_has_an_access_to_user_info() {
     let app = spawn_app().await;
     app.test_user.login(&app).await;
-    let response = app.post_user_info().await;
+    let response = app.get_user_info().await;
     let body = response.json::<UserInfo>().await.unwrap();
     assert_eq!(app.test_user.username, body.username);
     assert_eq!(app.test_user.user_id, body.user_id);
